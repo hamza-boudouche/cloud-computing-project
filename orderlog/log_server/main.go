3947dfbd59db5097156f5d3fd62cb03a815f9af9
hamza-boudouche <boudouche.hamza.13.07.2000@gmail.com>
2023-11-30 01:17:57 +0100

    orderlog microservice core implementation completed

--- orderlog/log_server/main.go
+++ orderlog/log_server/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
+    _ "github.com/lib/pq"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	pb "github.com/hamza-boudouche/orderlog/log"
 	"google.golang.org/grpc"
@@ -13,9 +17,48 @@ import (
 
 var (
 	port = flag.Int("port", 50051, "The server port")
+    dbUser = "log"
+    dbPassword = os.Getenv("POSTGRES_PASSWORD")
+    dbHost = os.Getenv("CLUSTER_EXAMPLE_2_RW_SERVICE_HOST")
+    dbPort = os.Getenv("CLUSTER_EXAMPLE_2_RW_PORT_5432_TCP_PORT")
+    dbName = "postgres"
+    connStr = fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbName)
+    db *sql.DB
 )
 
-// server is used to implement helloworld.GreeterServer.
+func init() {
+    // connection to default postgres db
+    var err error
+    db, err = sql.Open("postgres", connStr)
+    if err != nil {
+        fmt.Println("failed to connect to db")
+        log.Fatal(err)
+    }
+    fmt.Println("connected to db")
+    // creating log database
+    dbName = "log"
+    _, err = db.Query("create database " + dbName)
+    // connecting to log database
+    connStr = fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbName)
+    db, err = sql.Open("postgres", connStr)
+    fmt.Println("here")
+    if err != nil {
+        fmt.Println("failed to connect to new db")
+        log.Fatal(err)
+    }
+    fmt.Println("here")
+    // creating logs table if it doesn't exist
+    _, err = db.Exec("create table if not exists logs ( id serial primary key, contents varchar (200) )");
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func writeLog(db *sql.DB, contents string) error {
+    _, err := db.Exec("insert into logs(contents) values($1)", contents)
+    return err
+}
+
 type server struct {
     pb.UnimplementedLoggerServer
 }
@@ -23,6 +66,11 @@ type server struct {
 // SayHello implements helloworld.GreeterServer
 func (s *server) Log(ctx context.Context, in *pb.Entry) (*pb.Reply, error) {
 	log.Printf("Received: %v", in.GetContent())
+    // writing to db
+    err := writeLog(db, in.GetContent())
+    if err != nil {
+        return &pb.Reply{Ok: false}, errors.New("failed to write log into db")
+    }
     return &pb.Reply{Ok: true}, nil
 }
 
